e028cfea64cb3dbe0169f3cbe607f6fcf1acd533
cjnwan <cjnwan@gmail.com>
2015-04-17 19:51:16 +0800

    mvvm floders

---
+++ WindowsPhoneApp/WindowsPhoneApp.ZhihuDaily/ViewModel/ViewModelBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsPhoneApp.ZhihuDaily.ViewModel
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged,IDisposable
+    {
+        protected ViewModelBase() { }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
+
+        protected virtual void OnDispose()
+        {
+
+        }
+        public void Dispose()
+        {
+            this.OnDispose();
+        }
+    }
+}
